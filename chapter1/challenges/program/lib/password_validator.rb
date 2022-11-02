01a960abc2cea2256431418345ef44cb0580d9f2
Muhammad Azeem Patel <m.azeem786864@gmail.com>
2022-11-02 16:24:11 +0000

    ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,11 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    special = "!@$%&"
+    regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+    if password.length > 7 && password =~ regex
+        return true
+    else 
+        return false
+    end
 end
